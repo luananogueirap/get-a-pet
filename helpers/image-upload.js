4ee499703b3c544a94333b4cbf21bae1b7fa60b6
Luana Peixoto <luanapeixotosn@gmail.com>
2026-09-01 09:45:53 -0300

    image upload

---
+++ helpers/image-upload.js
@@ -0,0 +1,31 @@
+const multer = require('multer')
+const path = require('path')
+
+const imageStorage = multer.diskStorage({
+    destionation: function(req, file, cb){
+        let folder = ""
+
+        if(req.baseUrl.include('users')){
+            folder = 'users'
+        } else if(req.baseUrl.includes('pets')){
+            folder = 'pet'
+        }
+
+        cb(null, `public/images/${folder}`)
+    },
+    filename: function(req, file, cb){
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+})
+
+const imageUpload = multer({
+    storage: imageStorage,
+    fileFilter(req, file, cb){
+        if(!file.originalname.match(/\.(png|jpg)$/)){
+            return cb(new Error('please send just jpg or png'))
+        }
+        cb(undefined, true)
+    }
+})
+
+module.exports = {imageUpload}
